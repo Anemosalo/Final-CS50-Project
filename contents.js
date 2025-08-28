af18ac44eddc8e6b7adef636d7f391e07cd96b8b
anemos <anemosalouphs@gmai.com>
2025-08-28 17:16:20 +0300

    Added the slider but needs stylistic improvment

--- contents.js
+++ contents.js
@@ -1,10 +1,6 @@
 console.log("Loaded Content Side");
-const slider = document.getElementById("slider");
-slider.addEventListener('input', ()=> {
-    const value= slider.value;
-    slider.Value.textContent = value;
-    sliderContainer.style.setProperty('--progress',`$(value)$`);
-    sliderValue.style.left= `$(value)$`
-})
+
+
+
 
 
