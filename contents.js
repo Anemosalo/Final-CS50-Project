08e9f9cdca54220fc1eefb3662a58f6124776f0b
anemos <anemosalouphs@gmai.com>
2025-08-28 16:42:58 +0300

    Bug fixing

--- contents.js
+++ contents.js
@@ -1,5 +1,5 @@
 console.log("Loaded Content Side");
-
+const slider = document.getElementById("slider");
 slider.addEventListener('input', ()=> {
     const value= slider.value;
     slider.Value.textContent = value;
